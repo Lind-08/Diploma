9322251e706e48d946b533945c6c032ce4230341
lind-08 <lind8584@gmail.com>
2015-12-23 00:19:37 +0300

    new structure of catalogs

--- Linux Kонфигурация Samba_files/common_002.js
+++
@@ -1,4 +0,0 @@
-dojo._xdResourceLoaded(function(dojo, dijit, dojox){
-return {depends: [["provide", "dijit.nls.ru.common"]],
-defineResource: function(dojo, dijit, dojox){dojo.provide("dijit.nls.ru.common");dojo._xdLoadFlattenedBundle("dijit", "common", "ru", {"buttonOk":"ОК","buttonCancel":"Отмена","buttonSave":"Сохранить","itemClose":"Закрыть"});
-}};});
--- Linux Kонфигурация Samba_files/loading.js
+++
@@ -1,4 +0,0 @@
-dojo._xdResourceLoaded(function(dojo, dijit, dojox){
-return {depends: [["provide", "dijit.nls.ru.loading"]],
-defineResource: function(dojo, dijit, dojox){dojo.provide("dijit.nls.ru.loading");dojo._xdLoadFlattenedBundle("dijit", "loading", "ru", {"loadingState":"Загрузка...","errorState":"Извините, возникла ошибка"});
-}};});
